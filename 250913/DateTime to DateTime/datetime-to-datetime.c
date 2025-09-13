a6765c82d3087d361534d692bc0a2f29b34ac686
byoung-woo <146149322+byoung-woo@users.noreply.github.com>
2025-09-13 16:03:23 +0900

    Title: DateTime to DateTime, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 250913/DateTime to DateTime/datetime-to-datetime.c
+++ 250913/DateTime to DateTime/datetime-to-datetime.c
@@ -3,25 +3,12 @@
 int main() {
     int A, B, C;
     scanf("%d %d %d", &A, &B, &C);
-    
-    int time=0;
-    int minute, hour,day = 11;
 
-    while (1){
-        if(day==A && hour==B && minute==C)
-            break;
-        minute++;
-        time++;
-        if (minute == 60){
-            hour++;
-            minute=0;
-        }
-        if (hour == 24){
-            day++;
-            hour=0;
-        }
-    }
 
-    printf ("%d", time);
+    int diff = ((A * 24 * 60) + (B * 60) + C) - ((11 * 24 * 60) + (11 * 60) + 11);
+
+    printf("%d", diff);
+
+
     return 0;
 }
